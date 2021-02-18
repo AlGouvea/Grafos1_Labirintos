9b68a5d06fdee63928d031e967bd66fdf8e6db77
AlGouvea <alvarohsgouvea@gmail.com>
2021-02-18 13:20:10 -0300

    Initial definition of needed functions for maze generation

---
+++ gerar.js
@@ -0,0 +1,34 @@
+let labirinto = null;
+let res = 50;
+
+function setup(){
+
+}
+
+function draw(){
+
+}
+
+function geraLabirinto(w, h){
+
+}
+
+function labirintoIterar(){
+
+}
+
+function pickVizinho(bloco){
+
+}
+
+function paredeOposta(parede){
+
+}
+
+function drawlabirinto(){
+
+}
+
+function drawbloco(bloco, i, j){
+    
+}
